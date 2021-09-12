fc298702a3273fa10640255791f0a8595766de8c
BNB Punks <bnbpunksdev@gmail.com>
2021-09-12 13:01:31 -0700

    mma loot

--- src/components/ConnectMetamask/ConnectToMetamask.jsx
+++ src/components/ConnectMetamask/ConnectToMetamask.jsx
@@ -33,7 +33,7 @@ for (let i = 1; i < 4; i++) {
         var index = rand;
         var s = index+"";
         var newImageUrl = '/images/mmaloot' + s + '.png';
-        itemschar.push(<div class="card col-md-4" ><a href="https://app.nftrade.com/assets/bsc/0x48bf76b0dcc1326ae962b9301c40ee1ea399e186" ><img class="img-fluid" src={newImageUrl} /></a><div class="card-body"> <h5 class="card-title">NO {index} </h5></div></div>)
+        itemschar.push(<div class="card col-md-4" ><img class="img-fluid" src={newImageUrl} /><div class="card-body"> <h5 class="card-title">NO {index} </h5></div></div>)
 }
 
 const ConnectToMetamask = ({ connectToMetamask }) => {
