2958ff99dd1f34148b335dc90949210b223af116
BNB Punks <bnbpunksdev@gmail.com>
2021-11-13 15:02:22 -0800

    adding minting function

--- src/components/FormAndPreview/FormAndPreview.jsx
+++ src/components/FormAndPreview/FormAndPreview.jsx
@@ -34,7 +34,7 @@ class FormAndPreview extends Component {
       })
       this.state.punkOwner = tmp + "";
       tmp.then(
-        function(value) {this.state.punkOwner = value;},
+        function(value) {this.state.punkOwner = value + "";},
         function(error) {}
       );
 
