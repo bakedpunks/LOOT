d08cf1dced1ca452bcc6fc12e39beeab978a0545
BNB Punks <bnbpunksdev@gmail.com>
2021-11-13 16:20:21 -0800

    adding minting function

--- src/components/FormAndPreview/FormAndPreview.jsx
+++ src/components/FormAndPreview/FormAndPreview.jsx
@@ -20,9 +20,9 @@ class FormAndPreview extends Component {
 
 
 
-  Load_New_Image= async (e)=>{
+  Load_New_Image= (e)=>{
       this.state.punkid =e.target.value;
-      var tmp = this.props.punksOfferedForSale(this.state.punkid);
+      var tmp = await this.props.punksOfferedForSale(this.state.punkid);
       var s = this.state.punkid+"";
       while (s.length < 4) s = "0" + s;
 
