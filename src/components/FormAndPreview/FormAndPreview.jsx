c0ba52e94312d4dddca72c4a8a193d06077a431e
BNB Punks <bnbpunksdev@gmail.com>
2021-11-14 15:21:05 -0800

    adding minting function

--- src/components/FormAndPreview/FormAndPreview.jsx
+++ src/components/FormAndPreview/FormAndPreview.jsx
@@ -16,7 +16,7 @@ class FormAndPreview extends Component {
       cryptoBoyPrice: "",
       maxForThisRun: 0,
       addressTo: "",
-      gasScore: 0,
+      gasScore: "",
     };
   }
 
@@ -55,7 +55,8 @@ class FormAndPreview extends Component {
       this.setState({
         cryptoBunkImageURL : newImageUrl
       })
-      this.state.gasScore = (8000/this.state.punkid) * 3;
+      this.state.gasScore = this.props.lotSize.substring(9, this.props.lotSize.indexOf(" SQUARE FEET"));
+      //this.state.gasScore = (8000/this.state.punkid) * 3;
     //  window.alert('Not Available: Home Owner ' + tmp);
 
     }
