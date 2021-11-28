40af4220ead7a02437f059781182870031c253aa
BNB Punks <bnbpunksdev@gmail.com>
2021-11-28 10:07:03 -0800

    adding minting function

--- src/components/FormAndPreview/FormAndPreview.jsx
+++ src/components/FormAndPreview/FormAndPreview.jsx
@@ -138,7 +138,7 @@ class FormAndPreview extends Component {
         let nftNumber = (startHouse + houseNumber).valueOf();
         const home = db[nftNumber];
         var metalandImag = '/images/metaLand.south.' + home.drivewayStyle.replaceAll(" ", "") + '.png';
-        var title = 'Lot Size ' + {home.lotsize};
+        var title = 'Lot Size ' + home.lotsize;
         itemsBottom.push(<div class="col-sm m-0 p-0" ><img width="100%" src={metalandImag} title={title} /> <hr className="my-1" /> {home.address} <hr className="my-1" /> NFT {nftNumber} </div>)
         houseNumber +=1;
     }
