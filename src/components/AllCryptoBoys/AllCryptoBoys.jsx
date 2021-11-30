f23d32fc8ff80a5a9038a28f55556c0ff92fb441
BNB Punks <bnbpunksdev@gmail.com>
2021-11-29 17:04:48 -0800

    adding minting function

--- src/components/AllCryptoBoys/AllCryptoBoys.jsx
+++ src/components/AllCryptoBoys/AllCryptoBoys.jsx
@@ -66,31 +66,18 @@ class AllCryptoBoys extends Component {
 
     const items = []
 
-
-    for (const [index, value] of elements.entries()) {
-
-      var s = index+"";
-      var newImageUrl = '/images/loot-characters/' + s + '.png';
-      var newLinkUrl = '/mint?punkid=' + index;
-      items.push(<div class="card col-md-4" ><img src={newImageUrl} id={index} onClick={this.callViewPunkDetail} /><div class="card-body"> <h5 class="card-title">NO {index}</h5><p class="card-text"><small class="text-muted">OWNER {value}</small></p></div></div>)
-//      items.push(<Link to={newLinkUrl} className="nav-link" ><img src={newImageUrl} /></Link>)
-    }
     const itemsPage = items.splice(this.state.currentPage*500, 500);
     const pages = []
     const numberofPages = this.props.cryptoBoys.length/500;
-        for (let j=1;j<numberofPages;j++) {
-          var newLinkUrl = 'loadPage';
-          var k = j+10;
-          for ( j; j< k;j++) {
-              let nftNumber = j;
-              const home = db[j];
-              var metalandImag = '/images/metaLand.south.' + home.drivewayStyle.replaceAll(" ", "") + '.png';
-              var title = 'Lot Size ' + home.lotsize;
-              var divClass = 'col-sm m-0 p-0 bottom bg-primary text-center';
-              if(nftNumber ==this.state.punkid )divClass = 'col-sm m-0 p-0 bottom bg-primary bg-secondary text-white text-center';
-              pages[j] = <div class={divClass} ><img width="100%" src={metalandImag} title={title} id={nftNumber} value={nftNumber} onClick={this.Load_New_Image} /> <small><hr className="my-1" /> {home.address} </small></div>
-
-          }
+        for (let j=1;j<11;j++) {
+            var newLinkUrl = 'loadPage';
+            let nftNumber = j;
+            const home = db[j];
+            var metalandImag = '/images/metaLand.south.' + home.drivewayStyle.replaceAll(" ", "") + '.png';
+            var title = 'Lot Size ' + home.lotsize;
+            var divClass = 'col-sm m-0 p-0 bottom bg-primary text-center';
+            if(nftNumber ==this.state.punkid )divClass = 'col-sm m-0 p-0 bottom bg-primary bg-secondary text-white text-center';
+            pages[j] = <div class={divClass} ><img width="100%" src={metalandImag} title={title} id={nftNumber} value={nftNumber} onClick={this.Load_New_Image} /></div>
         }
 
     return (
@@ -99,11 +86,6 @@ class AllCryptoBoys extends Component {
       <div class="row m-0 p-0">
             {pages}
       </div>
-      </div>
-      <div class="container">
-    <div className="row">
-      {itemsPage}
-    </div>
       </div>
         <hr className="my-4" />
         BinanceLoot.io
