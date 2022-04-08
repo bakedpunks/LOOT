85f64fac3f5419d5fadfb016e066cbdcddd26c03
BNB Punks <bnbpunksdev@gmail.com>
2022-04-07 22:22:45 -0700

    webxr initial commit

--- src/components/ConnectMetamask/ConnectToMetamask.jsx
+++ src/components/ConnectMetamask/ConnectToMetamask.jsx
@@ -39,13 +39,41 @@ for (let i = 1; i < 4; i++) {
         itemsonchainmma.push(<div class="card col-md-4" ><a href="https://app.nftrade.com/assets/bsc/0x48bf76b0dcc1326ae962b9301c40ee1ea399e186" ><img class="img-fluid" src={newImageUrl} /></a><div class="card-body"> <h5 class="card-title">NO {index} </h5></div></div>)
 }
 
+const [isHovered, setIsHovered] = useState(false)
 
 const ConnectToMetamask = ({ connectToMetamask }) => {
-const [isHovered, setIsHovered] = useState(false);
   return (
-    <Interactive onSelect={() => console.log('clicked!')} onHover={() => setIsHovered(true)} onBlur={() => setIsHovered(false)}>
-      <Box />
-    </Interactive>
+    <div>
+      <div className="jumbotron">
+        <h1 className="display-5">
+          Metagascar
+        </h1>
+        The Metaverse is open, and the rocketships of explorers have been dispatched! The first MetaIsland to be discovered by the Alpha Explorer "Meta" has been dubbed Metagascar. Here you will find a thriving community of unique MetaHumans who reside in their equally unique MetaHomes. On the island of Metagascar you will find various facet's of social activity including friendship, dating, and of course what island would be complete without messages in a bottle!
+        <hr className="my-4" />
+        <button
+          onClick={connectToMetamask}
+          className="btn btn-primary d-flex align-items-center"
+          style={{ fontSize: "0.9rem", letterSpacing: "0.14rem" }}
+        >
+          Connect Wallet
+        </button>
+        <hr className="my-4" />
+        <img class="img-fluid" src="/images/main.logo.png" />
+        <hr className="my-4" />
+        <img class="img-fluid" src="/images/map.view.png" />
+
+
+
+      </div>
+
+      <div className="container">
+        <hr className="my-4" />
+            Metagascar &copy; 2021 Metagascar Project Inc. All rights reserved.
+        <hr className="my-4" />
+      </div>
+      <hr className="my-4" />
+
+    </div>
   );
 };
 
