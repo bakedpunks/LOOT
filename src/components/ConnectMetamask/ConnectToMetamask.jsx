50fe3649aab9f0ae93c9de316e16c1622b906ae4
BNB Punks <bnbpunksdev@gmail.com>
2022-04-13 01:12:47 -0700

    update react version

--- src/components/ConnectMetamask/ConnectToMetamask.jsx
+++ src/components/ConnectMetamask/ConnectToMetamask.jsx
@@ -73,7 +73,7 @@ const ConnectToMetamask = ({ connectToMetamask }) => {
 
           <a-box color="red" depth="2" height="4" width="0.5" position="0 0 -2"></a-box>
 
-          <Entity position="0 1.25 -1" gltf-model="{{src:'#earth'}}"></Entity>
+          <Entity position="0 1.25 -1" gltf-model="#earth"></Entity>
           <a-box position="-1 0.5 1" rotation="0 45 0" width="1" height="1" depth="1"  color="#4CC3D9"></a-box>
           <a-cylinder position="1 0.75 1" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
           <a-plane rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
