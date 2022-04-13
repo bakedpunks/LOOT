3bb5e4b56c981222dac98878db9ab40491e0fb88
BNB Punks <bnbpunksdev@gmail.com>
2022-04-13 01:03:18 -0700

    update react version

--- src/components/ConnectMetamask/ConnectToMetamask.jsx
+++ src/components/ConnectMetamask/ConnectToMetamask.jsx
@@ -6,7 +6,7 @@ import Web3 from 'web3';
 import { Entity, Scene } from "aframe-react";
 import "./styles.css";
 import "aframe";
-
+import "aframe-gltf-helpers"
 const items = []
 const itemsonchain = []
 const itemschar = []
@@ -68,7 +68,13 @@ const ConnectToMetamask = ({ connectToMetamask }) => {
       </div>
       <hr className="my-4" />
       <a-scene class="aframebox" embedded>
-          <a-entity position="0 1.25 -1" gltf-model="https://browniecoins-downloads.s3.us-east-2.amazonaws.com/modernvillahouse.v8.glb"></a-entity>
+          <a-assets>
+            <a-asset-item id="earth" src="https://browniecoins-downloads.s3.us-east-2.amazonaws.com/modernvillahouse.v8.glb"></a-asset-item>
+          </a-assets>
+
+          <a-box color="red" depth="2" height="4" width="0.5" position="0 0 -2"></a-box>
+            
+          <a-entity position="0 1.25 -1" gltf-model="{{src:'#earth'}}"></a-entity>
           <a-box position="-1 0.5 1" rotation="0 45 0" width="1" height="1" depth="1"  color="#4CC3D9"></a-box>
           <a-cylinder position="1 0.75 1" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
           <a-plane rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
