d263f1eba0a2e4565a17c06433209d2d312ce6c7
BNB Punks <bnbpunksdev@gmail.com>
2022-04-13 01:16:38 -0700

    update react version

--- src/components/ConnectMetamask/ConnectToMetamask.jsx
+++ src/components/ConnectMetamask/ConnectToMetamask.jsx
@@ -68,12 +68,12 @@ const ConnectToMetamask = ({ connectToMetamask }) => {
       <hr className="my-4" />
       <a-scene class="aframebox" embedded>
           <a-assets>
-            <a-asset-item id="earth" src="https://browniecoins-downloads.s3.us-east-2.amazonaws.com/skatepark.v3.glb"></a-asset-item>
+            <a-asset-item id="astronaut" src="https://cdn.glitch.com/ac5eecac-40b2-4897-8f67-28c497a19b47%2FAstronaut.glb"></a-asset-item>
           </a-assets>
+          <a-entity gltf-model="#astronaut" position="0 0 -2"></a-entity>
 
           <a-box color="red" depth="2" height="4" width="0.5" position="0 0 -2"></a-box>
 
-          <a-entity position="0 1.25 -1" gltf-model="#earth"></a-entity>
           <a-box position="-1 0.5 1" rotation="0 45 0" width="1" height="1" depth="1"  color="#4CC3D9"></a-box>
           <a-cylinder position="1 0.75 1" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
           <a-plane rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
