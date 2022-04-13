160fbf1c680e617e92fe81c945b74b8fe360406e
BNB Punks <bnbpunksdev@gmail.com>
2022-04-12 23:50:50 -0700

    update react version

--- src/components/ConnectMetamask/ConnectToMetamask.jsx
+++ src/components/ConnectMetamask/ConnectToMetamask.jsx
@@ -73,6 +73,24 @@ const ConnectToMetamask = ({ connectToMetamask }) => {
         <hr className="my-4" />
       </div>
       <hr className="my-4" />
+
+      Don McCurdy, Thank you for pointing me in the right direction. I was able to finally change the scene window size.
+      Here is the code to accomplish this.
+
+      HTML:
+
+      <a-scene class="aframebox" embedded>
+
+          <a-sphere position="0 1.25 -1" radius="1.25" color="#EF2D5E"></a-sphere>
+          <a-box position="-1 0.5 1" rotation="0 45 0" width="1" height="1" depth="1"  color="#4CC3D9"></a-box>
+          <a-cylinder position="1 0.75 1" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
+          <a-plane rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+
+          <a-sky color="#ECECEC"></a-sky>
+          <a-entity position="0 0 3.8">
+              <a-camera></a-camera>
+          </a-entity>
+      </a-scene>      
     </div>
   );
 };
