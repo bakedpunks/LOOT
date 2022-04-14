bd1feda395b184506a89c4b0d724c6acb250e0a8
BNB Punks <bnbpunksdev@gmail.com>
2022-04-13 21:44:35 -0700

    update react version

--- src/components/ConnectMetamask/ConnectToMetamask.jsx
+++ src/components/ConnectMetamask/ConnectToMetamask.jsx
@@ -6,6 +6,7 @@ import Web3 from 'web3';
 //import { Entity, Scene } from "aframe-react";
 import "./styles.css";
 import "aframe";
+import {Entity, Scene} from 'aframe-react';
 
 const items = []
 const itemsonchain = []
