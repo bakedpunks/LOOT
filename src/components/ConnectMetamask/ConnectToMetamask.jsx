1e96e11161d4b4b254b66c411447193eeef50bf8
BNB Punks <bnbpunksdev@gmail.com>
2022-04-14 14:50:45 -0700

    update react version

--- src/components/ConnectMetamask/ConnectToMetamask.jsx
+++ src/components/ConnectMetamask/ConnectToMetamask.jsx
@@ -80,9 +80,20 @@ const ConnectToMetamask = ({ connectToMetamask }) => {
       </div>
       <hr className="my-4" />
 
-      <a-scene class="aframebox" embedded>
+      <a-scene class="aframebox" embedded
+        environment={{
+          preset: 'starry',
+          seed: 2,
+          lightPosition: { x: 0.0, y: 0.03, z: -0.5 },
+          fog: 0.8,
+          ground: 'canyon',
+          groundYScale: 6.31,
+          groundTexture: 'walkernoise',
+          groundColor: '#8a7f8a',
+          grid: 'none'
+        }}
+        >
 
-              <Environment preset="tron" />
               {/*<Snow />*/}
          <AutoDetectControllers />
 
@@ -101,21 +112,7 @@ const ConnectToMetamask = ({ connectToMetamask }) => {
               </a-camera>
           </a-entity>
       </a-scene>
-
-      <Scene
-        environment={{
-          preset: 'starry',
-          seed: 2,
-          lightPosition: { x: 0.0, y: 0.03, z: -0.5 },
-          fog: 0.8,
-          ground: 'canyon',
-          groundYScale: 6.31,
-          groundTexture: 'walkernoise',
-          groundColor: '#8a7f8a',
-          grid: 'none'
-        }}
-      >
-      </Scene>      
+    
       <div className="container">
         <hr className="my-4" />
             Metagascar &copy; 2021 Metagascar Project Inc. All rights reserved.
