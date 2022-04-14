a6137aa4e156656b754e2e71aad35ac28e4620a7
BNB Punks <bnbpunksdev@gmail.com>
2022-04-13 21:47:48 -0700

    update react version

--- src/components/ConnectMetamask/ConnectToMetamask.jsx
+++ src/components/ConnectMetamask/ConnectToMetamask.jsx
@@ -69,7 +69,34 @@ const ConnectToMetamask = ({ connectToMetamask }) => {
       </div>
       <hr className="my-4" />
 
+      <a-scene cursor="rayOrigin: mouse" background="color: #FAFAFA">
+        <a-assets>
+          <a-asset-item
+            id="model"
+            src="https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b/Astronaut.glb?1542147958948"
+          />
+        </a-assets>
 
+        <Entity gltf-model={{ src: "#model" }} position="0 0 -5" />
+        <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9" shadow />
+        <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E" shadow />
+        <a-cylinder
+          position="1 0.75 -3"
+          radius="0.5"
+          height="1.5"
+          color="#FFC65D"
+          shadow=""
+        />
+        <a-plane
+          position="0 0 -4"
+          rotation="-90 0 0"
+          width="4"
+          height="4"
+          color="#7BC8A4"
+          shadow=""
+          add-number=""
+        />
+      </a-scene>
       <div className="container">
         <hr className="my-4" />
             Metagascar &copy; 2021 Metagascar Project Inc. All rights reserved.
