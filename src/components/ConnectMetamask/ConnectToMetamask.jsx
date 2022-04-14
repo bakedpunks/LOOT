64ab226ff15309e06cfde861b0280df0abea31ec
BNB Punks <bnbpunksdev@gmail.com>
2022-04-14 12:34:56 -0700

    update react version

--- src/components/ConnectMetamask/ConnectToMetamask.jsx
+++ src/components/ConnectMetamask/ConnectToMetamask.jsx
@@ -92,7 +92,8 @@ const ConnectToMetamask = ({ connectToMetamask }) => {
           primitive="a-box"
           color="red"
           position="0 0.5 -5"
-          event-set__click={ connectToMetamask }
+          event-set__click={connectToMetamask}
+          event-set__mouseenter="material.color: blue"
         />
           <a-entity position="0 0 3.8">
               <a-camera>
