d9bdf37c16484c4b50bb0c5bc07024f8fda22ed0
BNB Punks <bnbpunksdev@gmail.com>
2022-04-19 01:21:28 -0700

    metamask connect from a frame

--- src/components/ConnectMetamask/ConnectToMetamask.jsx
+++ src/components/ConnectMetamask/ConnectToMetamask.jsx
@@ -84,12 +84,15 @@ const ConnectToMetamask = ({ connectToMetamask }) => {
       <a-scene class="aframebox" embedded>
           <a-assets>
             <img id="my-image" src="https://view.metagascar.com/images/brick.wall.png" />
+            <a-asset-item id="cityModel" src="https://cdn.aframe.io/test-models/models/glTF-2.0/virtualcity/VC.gltf"></a-asset-item>
           </a-assets>
               <Environment preset="forest" />
               {/*<Snow />*/}
          <AutoDetectControllers />
 
          <a-image src="#my-image" width="3" height="1.5" position="0 0 1" ></a-image>
+
+         <a-entity gltf-model="#cityModel" modify-materials></a-entity>         
          // aframe-react's <Entity/> React Component
          <Entity geometry={{primitive: 'box', width: 5}} position="4 4 -5"/>
 
