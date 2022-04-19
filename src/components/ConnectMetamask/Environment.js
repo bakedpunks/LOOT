0524ee910beb91a2ea593e2ed4d7362c334c6d9d
BNB Punks <bnbpunksdev@gmail.com>
2022-04-19 09:06:28 -0700

    metamask connect from a frame

--- src/components/ConnectMetamask/Environment.js
+++ src/components/ConnectMetamask/Environment.js
@@ -6,6 +6,6 @@ import "aframe-environment-component";
 export default props => {
   var environment = props.environment || {};
   environment.preset = environment.preset || "default";
-  var environmentString = "{preset: forest}";
+  var environmentString = "{preset: tron}";
   return <a-entity environment={environmentString} />;
 };
