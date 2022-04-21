8a06af05e0cdde561d79b98dbdcf374420d13623
BNB Punks <bnbpunksdev@gmail.com>
2022-04-21 15:16:39 -0700

    metamask connect from a frame

--- src/components/ConnectMetamask/ConnectToMetamask.jsx
+++ src/components/ConnectMetamask/ConnectToMetamask.jsx
@@ -115,7 +115,7 @@ const ConnectToMetamask = ({ connectToMetamask }) => {
 
 
 
-
+         <a-image src="#wall" width="3" height="3" position="25 0 3" ></a-image>
          <a-image src="#wall" width="3" height="3" position="25 0 0" ></a-image>
          <a-image src="#wall" width="3" height="3" position="26.5 0 1.5" rotation="0 90 0" ></a-image>
 
