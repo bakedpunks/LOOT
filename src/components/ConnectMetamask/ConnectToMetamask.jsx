2ad5458fe6f2811cf9e663c600fddd1ab129340a
BNB Punks <bnbpunksdev@gmail.com>
2022-04-21 00:42:58 -0700

    metamask connect from a frame

--- src/components/ConnectMetamask/ConnectToMetamask.jsx
+++ src/components/ConnectMetamask/ConnectToMetamask.jsx
@@ -81,16 +81,10 @@ const ConnectToMetamask = ({ connectToMetamask }) => {
       class="aframebox"
       embedded
       networked-scene="
-      serverURL: '/';
-      app: 'metagascar';
-      room: 'metagascar';
-      connectOnLoad: true;
-      onConnect: 'onConnect';
-      adapter: 'wseasyrtc';
-      audio: false;
-      video: false;
-      debug: false;
-      "
+            room: dev;
+            debug: true;
+            adapter: wseasyrtc;
+     "
       >
           <a-assets>
             <img id="wall" src="https://view.metagascar.com/images/brick.wall.png" />
