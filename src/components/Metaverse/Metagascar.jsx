4c10a311696c56695b84f56d5bf7e63988d86fd4
BNB Punks <bnbpunksdev@gmail.com>
2022-04-23 17:09:06 -0700

    metamask connect from a frame

--- src/components/Metaverse/Metagascar.jsx
+++ src/components/Metaverse/Metagascar.jsx
@@ -9,7 +9,7 @@ import 'aframe';
 import 'aframe-event-set-component'; // NB: This enables event-set__click and such like, see https://www.npmjs.com/package/aframe-event-set-component
 import 'networked-aframe';
 
-import {mirror} from "aframe-mirror-component";
+import {mirror} from "./mirror.js";
 
 const items = []
 const itemsonchain = []
