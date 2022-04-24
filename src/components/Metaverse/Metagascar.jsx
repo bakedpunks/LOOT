8aec3bb5856275f893d11ce50f8ab2ff80ddda62
BNB Punks <bnbpunksdev@gmail.com>
2022-04-23 17:48:24 -0700

    metamask connect from a frame

--- src/components/Metaverse/Metagascar.jsx
+++ src/components/Metaverse/Metagascar.jsx
@@ -1,20 +1,101 @@
-import 'aframe';
-import 'aframe-particle-system-component';
-import {Entity, Scene} from 'aframe-react';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { PureComponent } from "react";
 
+import Web3 from 'web3';
+
+
+//import { Entity, Scene } from "aframe-react";
+
+import 'aframe';
+import 'aframe-event-set-component'; // NB: This enables event-set__click and such like, see https://www.npmjs.com/package/aframe-event-set-component
+import 'networked-aframe';
+
+import {mirror} from "aframe-mirror-component";
+
+function mouseEnter(control) {
+  console.log("mouseEnter: ", control);
+}
+
+function mouseLeave(control) {
+  console.log("mouseLeave: ", control);
+}
 
 
 const Metagascar = ({ connectToMetamask }) => {
   return (
-    <Scene>
-      <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}}/>
-      <Entity particle-system={{preset: 'snow'}}/>
-      <Entity light={{type: 'point'}}/>
-      <Entity gltf-model={{src: 'virtualcity.gltf'}}/>
-      <Entity text={{value: 'Hello, WebVR!'}}/>
-    </Scene>
+
+    <a-scene >
+        <a-assets>
+          <img id="wall" src="https://view.metagascar.com/images/brick.wall.png" />
+          <img id="street" src="https://view.metagascar.com/images/line.street.png" />
+          <a-asset-item id="crate-obj" src="https://view.metagascar.com/images/obj/modernvilla.obj"></a-asset-item>
+          <a-asset-item id="crate-mtl" src="https://view.metagascar.com/images/obj/modernvilla.mtl"></a-asset-item>
+          <template id="my-template">
+            <a-entity>
+              <a-sphere color="#f00"></a-sphere>
+            </a-entity>
+          </template>
+        </a-assets>
+
+       <a-entity environment="preset: tron"></a-entity>
+
+       <a-image src="#street" width="3" height="3" position="24 0.02 -3" rotation="90 0 0" ></a-image>
+       <a-image src="#street" width="3" height="3" position="21 0.02 -3" rotation="90 0 0" ></a-image>
+       <a-image src="#street" width="3" height="3" position="18 0.02 -3" rotation="90 0 0" ></a-image>
+       <a-image src="#street" width="3" height="3" position="15 0.02 -3" rotation="90 0 0" ></a-image>
+       <a-image src="#street" width="3" height="3" position="12 0.02 -3" rotation="90 0 0" ></a-image>
+       <a-image src="#street" width="3" height="3" position="9 0.02 -3" rotation="90 0 0" ></a-image>
+       <a-image src="#street" width="3" height="3" position="6 0.02 -3" rotation="90 0 0" ></a-image>
+       <a-image src="#street" width="3" height="3" position="3 0.02 -3" rotation="90 0 0" ></a-image>
+       <a-image src="#street" width="3" height="3" position="0 0.02 -3" rotation="90 0 0" ></a-image>
+       <a-image src="#street" width="3" height="3" position="-3 0.02 -3" rotation="90 0 0" ></a-image>
+       <a-image src="#street" width="3" height="3" position="-6 0.02 -3" rotation="90 0 0" ></a-image>
+       <a-image src="#street" width="3" height="3" position="-9 0.02 -3" rotation="90 0 0" ></a-image>
+       <a-image src="#street" width="3" height="3" position="-12 0.02 -3" rotation="90 0 0" ></a-image>
+       <a-image src="#street" width="3" height="3" position="-15 0.02 -3" rotation="90 0 0" ></a-image>
+       <a-image src="#street" width="3" height="3" position="-18 0.02 -3" rotation="90 0 0" ></a-image>
+       <a-image src="#street" width="3" height="3" position="-21 0.02 -3" rotation="90 0 0" ></a-image>
+
+
+
+       <a-image src="#wall" width="3" height="3" position="25 0 3" ></a-image>
+       <a-image src="#wall" width="3" height="3" position="25 0 0" ></a-image>
+       <a-image src="#wall" width="3" height="3" position="26.5 0 1.5" rotation="0 90 0" ></a-image>
+
+       <a-image src="#wall" width="3" height="3" position="20 0 0" ></a-image>
+       <a-image src="#wall" width="3" height="3" position="15 0 0" ></a-image>
+       <a-image src="#wall" width="3" height="3" position="10 0 0" ></a-image>
+       <a-image src="#wall" width="3" height="3" position="5 0 0" ></a-image>
+       <a-image src="#wall" width="3" height="3" position="0 0 0" ></a-image>
+
+       <a-image src="#wall" width="3" height="3" position="-5 0 0" ></a-image>
+       <a-image src="#wall" width="3" height="3" position="-10 0 0" ></a-image>
+       <a-image src="#wall" width="3" height="3" position="-15 0 0" ></a-image>
+       <a-image src="#wall" width="3" height="3" position="-20 0 0" ></a-image>
+
+
+       <a-entity obj-model="obj:#crate-obj"
+                 material="color: green" position="0 0 -4.5" scale="0.02 0.02 0.04" rotation="0 -82 0" >
+       </a-entity>
+       <a-entity obj-model="obj:#crate-obj"
+                 material="color: blue" position="15 0 -4.5" scale="0.02 0.02 0.04" rotation="0 -82 0" >
+       </a-entity>
+
+       <a-sphere mirror="resolution: 64; distance: 1000; interval: 200; repeat: true" segments-radius="24" segments-height="24" radius="1.5" position="0 2.5 4" ></a-sphere>
+
+       <a-entity id="mirror"
+                 scale="1.0 1.0 1.0"
+                 rotation="0 0 0"
+                 position="0.0 1.5 -0.75"
+                 geometry="primitive:plane; width:1.6; height:2.0"
+                 mirror >
+       </a-entity>
+
+        <a-entity position="0 0 3.8" network="template:#my-template;attachTemplateToLocal:false;" >
+            <a-camera >
+              <a-cursor />
+            </a-camera>
+        </a-entity>
+    </a-scene>
 
   );
 };
