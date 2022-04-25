886299cd64dec4811810ce593eeb472c323edf42
BNB Punks <bnbpunksdev@gmail.com>
2022-04-25 00:53:27 -0700

    metamask connect from a frame

--- src/components/Metaverse/Metagascar.jsx
+++ src/components/Metaverse/Metagascar.jsx
@@ -28,7 +28,7 @@ const items = []
 for (let k = 1; k < 11; k++) {
     let nftNumber = (k).valueOf();
     const home = db[nftNumber];
-    var text_value = "value: " + home.address + "; side: double" ;
+    var text_value = "value: " + home.address + "; side: front" ;
     var position = (k) + " 0.5 " + (0);
     var position_street = (k) + " 0.02 " + (0.5);
     var position_text = (k) + " 1 " + (0);
@@ -38,7 +38,7 @@ for (let k = 1; k < 11; k++) {
     position = (k) + " 0.5 " + (1);
     position_text = (k) + " 1 " + (1);
     home = db[nftNumber + 10];
-    text_value = "value: " + home.address + ";" ;
+    text_value = "value: " + home.address + "; side: back" ;
     items.push(<a-image src="#wall" width="1" height="1" position={position} ></a-image>)
     items.push(<a-entity text={text_value} position={position_text} scale="1 1 1" ></a-entity>)
 }
