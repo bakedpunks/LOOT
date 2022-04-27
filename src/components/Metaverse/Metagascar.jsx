8b5e9d177d4953e15b58928c3ab3afbaefc1ec55
BNB Punks <bnbpunksdev@gmail.com>
2022-04-27 14:42:02 -0700

    error my properties page

--- src/components/Metaverse/Metagascar.jsx
+++ src/components/Metaverse/Metagascar.jsx
@@ -53,7 +53,7 @@ for (let j = 0; j < 400; j++) {
 const Metagascar = ({ connectToMetamask }) => {
   return (
 
-    <a-scene networked-scene="serverURL: ws://149.248.10.154:5001; app: metagascar; room: metagascar; connectOnLoad: true; onConnect: onConnect; adapter: wseasyrtc; audio: false; video: false; debug: true;" >
+    <a-scene networked-scene="serverURL: ws://view.metagascar.com; app: metagascar; room: metagascar; connectOnLoad: true; onConnect: onConnect; adapter: wseasyrtc; audio: false; video: false; debug: true;" >
         <a-assets>
           <img id="wall" src="https://view.metagascar.com/images/house.png" />
           <img id="street" src="https://view.metagascar.com/images/line.street.png" />
