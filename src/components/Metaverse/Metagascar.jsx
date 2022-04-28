8b84af199e99e34f61f3d1431c7c03573d2304a8
BNB Punks <bnbpunksdev@gmail.com>
2022-04-27 17:49:31 -0700

    error my properties page

--- src/components/Metaverse/Metagascar.jsx
+++ src/components/Metaverse/Metagascar.jsx
@@ -9,11 +9,13 @@ import 'aframe';
 import 'aframe-event-set-component'; // NB: This enables event-set__click and such like, see https://www.npmjs.com/package/aframe-event-set-component
 import 'networked-aframe';
 import adapter from 'webrtc-adapter';
-
+import { Server } from "socket.io";
 import {mirror} from "aframe-mirror-component";
 
 
 import db  from "../../database";
+
+const io = new Server(server);
 
 function mouseEnter(control) {
   console.log("mouseEnter: ", control);
